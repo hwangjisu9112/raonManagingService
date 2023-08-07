2c2e43c420ac46c126fec1145694f51de7744aaf
JISU\crist <crist9112@naver.com>
2023-08-07 19:18:31 +0900

    0807

--- manager/src/main/java/com/example/raon/invoice/InvoiceController.java
+++ manager/src/main/java/com/example/raon/invoice/InvoiceController.java
@@ -1,11 +1,15 @@
 package com.example.raon.invoice;
 
+import java.io.IOException;
 import java.security.Principal;
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +19,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.example.raon.customer.Customer;
 import com.example.raon.employee.Employee;
 
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import com.itextpdf.layout.Document;
 
+import com.itextpdf.html2pdf.ConverterProperties;
+import com.itextpdf.html2pdf.HtmlConverter;
+import com.itextpdf.io.source.ByteArrayOutputStream;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.text.DocumentException;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 import lombok.RequiredArgsConstructor;
@@ -29,6 +51,11 @@ public class InvoiceController {
 
 	private final InvoiceService invoiceService;
 	private final InvoiceRepository invoiceRepository;
+	private final TemplateEngine templateEngine; 
+
+	
+    Logger logger = Logger.getLogger(getClass().getName());
+
 
 	@GetMapping("/board")
 	public String BoardInvoice(Model model) {
@@ -77,6 +104,40 @@ public class InvoiceController {
 
 		this.invoiceService.delete(invoice);
 		return "redirect:/invoice/board";
+	}
+	
+
+	@PostMapping("/generatePdf")
+	public ResponseEntity<ByteArrayResource> generatePdfPost(@ModelAttribute("invoice") Invoice invoice, HttpServletResponse response) throws DocumentException, IOException {
+
+	    logger.log(Level.INFO, "Generating PDF for invoice: " + invoice);
+
+	    // Create a ByteArrayOutputStream to hold PDF content
+	    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+	    // Create a PDF document
+	    PdfDocument pdfDocument = new PdfDocument(new PdfWriter(outputStream));
+	    Document document = new Document(pdfDocument);
+
+	    String htmlContent = templateEngine.process("invoice_view", new Context(Locale.getDefault(), Collections.singletonMap("invoice", invoice)));
+
+	    logger.log(Level.INFO, "HTML content: " + htmlContent);
+
+	    // Convert HTML to PDF using iText 7's HtmlConverter
+	    ConverterProperties converterProperties = new ConverterProperties();
+	    HtmlConverter.convertToPdf(htmlContent, pdfDocument, converterProperties);
+
+	    // Close the document
+	    document.close();
+
+	    // Get the PDF bytes from the ByteArrayOutputStream
+	    byte[] pdfBytes = outputStream.toByteArray();
+
+	    // Return the PDF file to the client
+	    return ResponseEntity.ok()
+	            .contentType(MediaType.APPLICATION_PDF)
+	            .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=invoice.pdf")
+	            .body(new ByteArrayResource(pdfBytes));
 	}
 	
 	
